b682644a24100dc34526e0a628ab7917453f9113
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-09-22 02:31:28 +0530

    Init

--- form.php
+++ form.php
@@ -52,10 +52,10 @@ if(isset($_POST['name'])){
     <link rel="stylesheet" href="form.css">
 </head>
 <body>
-    <img class="form" src="form.jpg" alt="IIT Kharagpur">
+    <img class="form" src="form.jpg" alt="Form">
     <div class="container">
-        <h1>Welcome to IIT Kharagpur US Trip form</h3>
-        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <h1>Welcome to Student Performance Tracker form</h3>
+        <p>Enter your details and submit this form </p>
         <?php
         if($insert == true){
         echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
