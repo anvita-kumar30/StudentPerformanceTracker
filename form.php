925c3c8782ea00008ae659f892bf8a8e2acd184f
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-09-28 18:33:37 +0530

    Init

--- form.php
+++ form.php
@@ -54,14 +54,14 @@ if(isset($_POST['name'])){
 <body>
     <img class="form" src="form.jpg" alt="Form">
     <div class="container">
-        <h1>Welcome to Student Performance Tracker form</h3>
+        <h1>Welcome to Student Performance Tracker</h3>
         <p>Enter your details and submit this form </p>
         <?php
         if($insert == true){
         echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
         }
         ?>
-        <form action="index.php" method="post">
+        <form action="form.php" method="post">
             <input type="text" name="name" id="name" placeholder="Enter your name">
             <input type="text" name="age" id="age" placeholder="Enter your Age">
             <input type="text" name="gender" id="gender" placeholder="Enter your gender">
