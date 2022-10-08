7413c32605730bf5abdfa08c9ba75fb379f757bd
sanjanalalwani <610sanjana@gmail.com>
2022-10-08 16:26:05 +0530

    init

--- form.php
+++ form.php
@@ -61,12 +61,14 @@ if(isset($_POST['name'])){
         echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
         }
         ?>
-        <form action="form.php" method="post">
+       <form action="index.php" method="post">
             <input type="text" name="name" id="name" placeholder="Enter your name">
-            <input type="text" name="age" id="age" placeholder="Enter your Age">
-            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-            <input type="email" name="email" id="email" placeholder="Enter your email">
-            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <input type="text" name="marks" id="marks" placeholder="Enter your JAVA marks">
+            <input type="text" name="marks" id="marks" placeholder="Enter your DLCA marks">
+            <input type="text" name="marks" id="marks" placeholder="Enter your DSGT marks">
+            <input type="text" name="marks" id="marks" placeholder="Enter your DS marks">
+            <input type="text" name="marks" id="marks" placeholder="Enter your MATH marks">
+            <input type="text" name="marks" id="marks" placeholder="Enter your CG marks">
             <textarea name="desc" id="desc" cols="30" rows="6" placeholder="Enter any other information here"></textarea>
             <button class="btn">Submit</button> 
         </form>
