58758644073b9bee890b4f78ffd4607da12d6e8b
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-10-09 15:45:10 +0530

    init

--- welcome.php
+++ welcome.php
@@ -20,12 +20,12 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!==true)
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Php login system</title>
+    <title>Login system</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-darks bg-dark">
-        <a class="navbar-brand" href="#">Php login system</a>
+        <a class="navbar-brand" href="#">Login system</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
