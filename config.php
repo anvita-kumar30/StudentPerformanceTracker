5272365600ce84424aa50fe5a4cfea946d95ee53
sanjanalalwani <610sanjana@gmail.com>
2022-11-11 13:17:10 +0530

    init

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+define('DB_NAME', 'miniproject');
 
 //Try connecting to the database
 $conn=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
